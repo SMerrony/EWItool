85ab04bbdc85ea1bfddded8fcfc4e52d0a447d3f
Stephen Merrony <SMerrony@users.noreply.github.com>
2015-12-02 08:32:32 +0000

    Create setters.

--- EWItool2/src/ewitool/Prefs.java
+++ EWItool2/src/ewitool/Prefs.java
@@ -22,7 +22,7 @@ import java.util.prefs.Preferences;
 
 public class Prefs {
   
-  public static final String PREFS_NODE = "EWItool";
+  public static final String PREFS_NODE = "ewitool";
   public static final String MIDI_IN_PORT = "MIDI_IN_PORT";
   public static final String MIDI_OUT_PORT = "MIDI_OUT_PORT";
   public static final String LIBRARY_LOCATION = "LIBRARY_LOCATION";
@@ -31,32 +31,56 @@ public class Prefs {
   public static final String EPX_PASSWORD = "EPX_PASSWORD";
   
   public static String getMidiInPort() {
-    Preferences p = Preferences.systemRoot().node( PREFS_NODE );
+    Preferences p = Preferences.userRoot().node( PREFS_NODE );
     return p.get( MIDI_IN_PORT, "" );
   }
+  public static void setMidiInPort( String ip ) {
+    Preferences p = Preferences.userRoot().node( PREFS_NODE );
+    p.put( MIDI_IN_PORT, ip );
+  }
   public static String getMidiOutPort() {
-    Preferences p = Preferences.systemRoot().node( PREFS_NODE );
+    Preferences p = Preferences.userRoot().node( PREFS_NODE );
     return p.get( MIDI_OUT_PORT, "" );
   }
+  public static void setMidiOutPort( String op ) {
+    Preferences p = Preferences.userRoot().node( PREFS_NODE );
+    p.put( MIDI_OUT_PORT, op );
+  }
   public static String getLibraryLocation() {
-    Preferences p = Preferences.systemRoot().node( PREFS_NODE );
+    Preferences p = Preferences.userRoot().node( PREFS_NODE );
     return p.get( LIBRARY_LOCATION, "" );
   }
+  public static void setLibraryLocation( String ll ) {
+    Preferences p = Preferences.userRoot().node( PREFS_NODE );
+    p.put( LIBRARY_LOCATION, ll );
+  }
   public static String getEpxHost() {
-    Preferences p = Preferences.systemRoot().node( PREFS_NODE );
+    Preferences p = Preferences.userRoot().node( PREFS_NODE );
     return p.get( EPX_HOST, "" );
   }
+  public static void setEpxHost( String host ) {
+    Preferences p = Preferences.userRoot().node( PREFS_NODE );
+    p.put( EPX_HOST, host );
+  }
   public static String getEpxUserid() {
-    Preferences p = Preferences.systemRoot().node( PREFS_NODE );
+    Preferences p = Preferences.userRoot().node( PREFS_NODE );
     return p.get( EPX_USERID, "" );
   }
+  public static void setEpxUserid( String user ) {
+    Preferences p = Preferences.userRoot().node( PREFS_NODE );
+    p.put( EPX_USERID, user );
+  }
   public static String getEpxPassword() {
-    Preferences p = Preferences.systemRoot().node( PREFS_NODE );
+    Preferences p = Preferences.userRoot().node( PREFS_NODE );
     return p.get( EPX_PASSWORD, "" );
+  }
+  public static void setEpxPassword( String pwd ) {
+    Preferences p = Preferences.userRoot().node( PREFS_NODE );
+    p.put( EPX_PASSWORD, pwd );
   }
   
   public boolean clearPrefs() {
-    Preferences p = Preferences.systemRoot().node( PREFS_NODE );
+    Preferences p = Preferences.userRoot().node( PREFS_NODE );
     try {
       p.clear();
     } catch( BackingStoreException e ) {
