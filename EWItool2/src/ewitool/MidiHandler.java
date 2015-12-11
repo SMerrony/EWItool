abd5f1aab70983739c9e371070ef0456969856d1
Stephen Merrony <SMerrony@users.noreply.github.com>
2015-12-11 17:18:17 +0000

    Initial commit.

---
+++ EWItool2/src/ewitool/MidiHandler.java
@@ -0,0 +1,218 @@
+/**
+ * MidiHandler - This class controls the MIDI connections and their
+ * respective tasks.
+ * 
+ * Pseudocode:
+ * 
+
+ *   start subtasks if MIDI ports set
+ *   else
+ *     wait for signal
+ *     switch( signal-type ) 
+ *       case midi-in-set: (re)start MidiReceiver task
+ *       case midi-out-set: (re)start MidiSender task
+ *       case stop: stop Midi tasks
+ *     end-switch
+ * end-loop
+ * 
+ * This file is part of EWItool.
+
+    EWItool is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    EWItool is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with EWItool.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package ewitool;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Receiver;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.Synthesizer;
+import javax.sound.midi.SysexMessage;
+
+public class MidiHandler {
+   
+  //SysEx commands from Akai
+  public final static byte MIDI_PRESET_DUMP      = 0x00;
+  public final static byte MIDI_PRESET_DUMP_REQ  = 0x40;
+  public final static byte MIDI_QUICKPC_DUMP     = 0x01;
+  public final static byte MIDI_QUICKPC_DUMP_REQ = 0x41;
+  public final static byte MIDI_EDIT_LOAD        = 0x10;
+  public final static byte MIDI_EDIT_STORE       = 0x11;
+  public final static byte MIDI_EDIT_DUMP        = 0x20;
+  public final static byte MIDI_EDIT_DUMP_REQ    = 0x60;
+
+  public final static byte MIDI_SYSEX_HEADER     = (byte) 0b11110000; // 0xf0
+  public final static byte MIDI_SYSEX_TRAILER    = (byte) 0b11110111; // 0xf7
+  public final static byte MIDI_SYSEX_AKAI_ID    = 0x47; 
+  public final static byte MIDI_SYSEX_AKAI_EWI4K = 0x64;
+  public final static byte MIDI_SYSEX_CHANNEL    = 0x00;
+  public final static byte MIDI_SYSEX_ALLCHANNELS = 0x7f;
+
+  public final static byte MIDI_CC_DATA_ENTRY    = 0x06;
+  public final static byte MIDI_CC_NRPN_LSB      = 0x62;
+  public final static byte MIDI_CC_NRPN_MSB      = 0x63;
+  
+  public final static int  MAX_SYSEX_LENGTH      = 262144;
+  public final static int  EWI_SYSEX_PRESET_DUMP_LEN = EWI4000sPatch.EWI_PATCH_LENGTH;
+  public final static int  EWI_SYSEX_QUICKPC_DUMP_LEN = 91;
+  public final static int  MIDI_TIMEOUT_MS       = 3000;
+  
+  SharedData sharedData;
+  Prefs userPrefs;
+  MidiDevice inDev = null, outDev = null; 
+  
+  MidiHandler( SharedData pSharedData, Prefs pUserPrefs ) {
+    
+    sharedData = pSharedData;
+    userPrefs = pUserPrefs;
+     
+    MidiDevice device;
+    Receiver midiIn;
+    MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
+    for (int d = 0; d < infos.length; d++) {
+      try {
+        device = MidiSystem.getMidiDevice( infos[d] );
+        if (!(device instanceof Sequencer) && !(device instanceof Synthesizer)) {
+          if (device.getMaxReceivers() != 0) {
+            if (infos[d].getName().equals( userPrefs.midiOutPort.getValue() )) {
+              outDev = MidiSystem.getMidiDevice( infos[d] );
+              outDev.open();
+              //midiOut = new MidiSender();
+              System.out.println( "Debug - OUT Port: " + infos[d].getName());
+            }
+          } else if (device.getMaxTransmitters() != 0) {
+            if (infos[d].getName().equals( userPrefs.midiInPort.getValue() )) {
+              inDev = MidiSystem.getMidiDevice( infos[d] );
+              inDev.open(); 
+              midiIn = new MidiReceiver( sharedData );
+              inDev.getTransmitter().setReceiver( midiIn );
+              System.out.println( "Debug - IN Port: " + infos[d].getName() );
+            }
+          }
+        }  
+      } catch (Exception e) {
+        System.err.println( "ERROR - Fetching MIDI information" );
+      }
+    }
+
+  }
+  
+  public void close() {
+    outDev.close();
+    inDev.close();
+  }
+
+  /**
+   * 
+   */
+  public void restart() {
+
+    close();
+    
+    MidiDevice device;
+    Receiver midiIn;
+    MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
+    for (int d = 0; d < infos.length; d++) {
+      try {
+        device = MidiSystem.getMidiDevice( infos[d] );
+        if (!(device instanceof Sequencer) && !(device instanceof Synthesizer)) {
+          if (device.getMaxReceivers() != 0) {
+            if (infos[d].getName().equals( userPrefs.midiOutPort.getValue() )) {
+              outDev = MidiSystem.getMidiDevice( infos[d] );
+              outDev.open();
+              //midiOut = new MidiSender();
+              System.out.println( "Debug - OUT Port: " + infos[d].getName());
+            }
+          } else if (device.getMaxTransmitters() != 0) {
+            if (infos[d].getName().equals( userPrefs.midiInPort.getValue() )) {
+              inDev = MidiSystem.getMidiDevice( infos[d] );
+              inDev.open();
+              midiIn = new MidiReceiver( sharedData );
+              inDev.getTransmitter().setReceiver( midiIn );
+              System.out.println( "Debug - IN Port: " + infos[d].getName() );
+            }
+          }
+        }  
+      } catch (Exception e) {
+        System.err.println( "ERROR - Fetching MIDI information" );
+      }
+    }
+
+  }
+  
+  void clearPatches() {
+    sharedData.clear();
+  }
+  
+  void requestPatch( int p ) {
+    
+    if (p < 0 || p > EWI4000sPatch.EWI_NUM_PATCHES) {
+      System.err.println( "Error - Attempt to request out-of-range patch (" + p + ")" );
+      System.exit( 1 );
+    }
+    
+    sharedData.ewiPatches[p] = new EWI4000sPatch();
+    
+    byte[] reqMsg = new byte[6];
+    //reqMsg[0] = MIDI_SYSEX_HEADER;
+    reqMsg[0] = MIDI_SYSEX_AKAI_ID;
+    reqMsg[1] = MIDI_SYSEX_AKAI_EWI4K;
+    reqMsg[2] = MIDI_SYSEX_CHANNEL;
+    reqMsg[3] = MIDI_PRESET_DUMP_REQ;
+    reqMsg[4] = (byte) p;
+    reqMsg[5] = MIDI_SYSEX_TRAILER;
+    SysexMessage sysEx;
+    try {
+      sysEx = new SysexMessage( SysexMessage.SYSTEM_EXCLUSIVE, reqMsg, reqMsg.length );
+      outDev.getReceiver().send( sysEx, -1 );
+      System.out.println( "DEBUG - Requested patch: " + p );
+
+      while ( sharedData.ewiPatches[p].name[0] == 0 ) {} // FIXME Yuck, yuck, yuck!
+
+      
+    } catch( InvalidMidiDataException e ) {
+      e.printStackTrace();
+    } catch( MidiUnavailableException e ) {
+      e.printStackTrace();
+    }
+
+  }
+  
+  void requestQuickPCs() {
+    
+    byte[] reqMsg = new byte[6];
+    // reqMsg[0] = MIDI_SYSEX_HEADER;
+    reqMsg[0] = MIDI_SYSEX_AKAI_ID;
+    reqMsg[1] = MIDI_SYSEX_AKAI_EWI4K;
+    reqMsg[2] = MIDI_SYSEX_ALLCHANNELS;
+    reqMsg[3] = MIDI_QUICKPC_DUMP_REQ;
+    reqMsg[4] = 0x00;
+    reqMsg[5] = MIDI_SYSEX_TRAILER;
+    SysexMessage sysEx;
+    try {
+      sysEx = new SysexMessage( SysexMessage.SYSTEM_EXCLUSIVE, reqMsg, reqMsg.length );
+      outDev.getReceiver().send( sysEx, -1 );
+    } catch( InvalidMidiDataException e ) {
+      e.printStackTrace();
+    } catch( MidiUnavailableException e ) {
+      e.printStackTrace();
+    }
+
+  }
+
+  
+}
---
+++ EWItool2/src/ewitool/MidiReceiver.java
@@ -0,0 +1,81 @@
+/**
+ * This file is part of EWItool.
+ *
+ *  EWItool is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  EWItool is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with EWItool.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ewitool;
+
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.Receiver;
+import javax.sound.midi.SysexMessage;
+
+public class MidiReceiver implements Receiver {
+ 
+  SharedData sharedData;
+
+  /**
+   * @param ewiPatches
+   */
+  public MidiReceiver( SharedData pSharedData ) {
+    sharedData = pSharedData;
+  }
+
+  @Override
+  public void send( MidiMessage message, long timeStamp ) {
+
+    // we are currently only interested in SysEx messages from the EWI
+    if (message instanceof SysexMessage) {
+      byte[] messageBytes = ((SysexMessage) message).getData();
+      switch( messageBytes[3] ) {
+      case MidiHandler.MIDI_PRESET_DUMP:
+        if (messageBytes.length != (MidiHandler.EWI_SYSEX_PRESET_DUMP_LEN - 1)) {
+          System.err.println( "Error - Invalid preset dump SysEx received from EWI");
+          return;
+        }
+        EWI4000sPatch thisPatch = new EWI4000sPatch();
+        for (int b = 0; b < (MidiHandler.EWI_SYSEX_PRESET_DUMP_LEN - 1); b++)
+          thisPatch.patch_blob[b+1] = messageBytes[b];
+        //thisPatch.patch_blob = messageBytes;
+        thisPatch.decodeBlob();
+        if (thisPatch.header[3] == MidiHandler.MIDI_SYSEX_ALLCHANNELS) {
+          int thisPatchNum = (int) thisPatch.patch_num; // FIXME ++?
+          if (thisPatchNum < 0 || thisPatchNum >= EWI4000sPatch.EWI_NUM_PATCHES) {
+            System.err.println( "Error - Invalid patch number (" + thisPatchNum + ") received from EWI");
+          } else {
+            sharedData.ewiPatches[thisPatchNum] = thisPatch;
+            sharedData.ewiPatches[thisPatchNum].decodeBlob();
+            sharedData.setLastPatchLoaded( thisPatchNum );
+            // System.out.println( "DEBUG - MidiReceiver: Patch number: " + thisPatchNum + " received" );
+          }
+        }
+        break;
+      case MidiHandler.MIDI_QUICKPC_DUMP:
+        break;
+      default:
+        System.err.println( "Warning - Unrecognised SysEx type (" + messageBytes[4] + ") received from EWI");
+        break;
+      }
+    }
+
+  }
+
+  @Override
+  public void close() {
+    
+  }
+
+
+ 
+}
