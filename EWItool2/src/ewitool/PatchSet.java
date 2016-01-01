920f7695b3d872564e5189c6afe2f59a86c929de
Stephen Merrony <steve@stephenmerrony.co.uk>
2016-01-01 12:23:44 +0100

    New class to represent (actions on) sets of patches.

---
+++ EWItool2/src/ewitool/PatchSet.java
@@ -0,0 +1,53 @@
+/**
+ * This file is part of EWItool.
+ *
+ *  EWItool is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  EWItool is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with EWItool.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ewitool;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+
+public final class PatchSet {
+  
+  public enum Status { OK, ALREADY_EXISTS, NO_PERMISSION }
+  
+  public static Status save( LinkedList<EWI4000sPatch> patchList, String filename ) throws IOException {
+    
+    UserPrefs prefs = new UserPrefs();
+    Path path = Paths.get( prefs.getLibraryLocation(), filename );
+    if (Files.exists( path )) return Status.ALREADY_EXISTS;
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    for ( EWI4000sPatch p : patchList) {
+      bos.write( p.patchBlob );
+    }
+    byte[] bytes = bos.toByteArray();
+    try {
+      Files.write( path, bytes );
+    } catch( FileAlreadyExistsException e) {
+      return Status.ALREADY_EXISTS;
+    } catch( IOException e ) {
+      return Status.NO_PERMISSION;
+    }
+    
+    return Status.OK;
+  }
+
+}
